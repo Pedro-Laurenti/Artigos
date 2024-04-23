44b59824428ed17daeadd690d692293005500dee
Pedro Laurenti <pedrolaurenti04@gmail.com>
2024-04-23 18:31:29 -0300

    SUCESS

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,5 +1,5 @@
 /**
-* This configuration file lets you run `$ sanity [command]` in this folder
+* This confaiguration file lets you run `$ sanity [command]` in this folder
 * Go to https://www.sanity.io/docs/cli to learn more.
 **/
 import { defineCliConfig } from 'sanity/cli'

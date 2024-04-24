81ed0fe3c2c430c2e45760235d5440e8e6aeec9a
Pedro Laurenti <pedrolaurenti04@gmail.com>
2024-04-24 13:39:43 -0300

    .

--- src/app/(content)/articles/[slug]/opengraph-image.tsx
+++ src/app/(content)/articles/[slug]/opengraph-image.tsx
@@ -1,37 +1,35 @@
 /* eslint-disable @next/next/no-img-element */
-import { ImageResponse } from "next/server";
+import { ImageResponse } from "next/og"; // Updated import path
 import { urlFor } from "@/utils/sanity-utils";
-import { postQuery} from "@/sanity/lib/queries";
+import { postQuery } from "@/sanity/lib/queries";
 import { sanityFetch } from "@/sanity/lib/sanityFetch";
 import { SanityDocument } from "@sanity/client";
 
 export const size = {
-    width: 1300,
-    height: 727,
+  width: 1300,
+  height: 727,
 };
 
-export const contentType = "image/*";
+export const contentType = "image/*"; // Corrected line
 
 interface Props {
-    params: {
-        slug: string;
-    };
+  params: {
+    slug: string;
+  };
 }
 
-export default async function og({ params }: Props) {
-    const post = await sanityFetch<SanityDocument>({
-        query: postQuery,
-        params,
-    });
+export default async function og({ params }: Props): Promise<ImageResponse> {
+  const post = await sanityFetch<SanityDocument>({
+    query: postQuery,
+    params,
+  });
 
-    return new ImageResponse(
-        (
-            <div tw='relative flex items-center justify-center'>
-                <img
-                    src={`${urlFor(post?.mainImage?.asset?._ref)}`}
-                    alt={post?.title}
-                />
-            </div>
-        )
-    );
+  // Assuming ImageResponse has a `body` property (adjust if different)
+  const imageResponse = new ImageResponse(
+    post?.mainImage?.asset?._ref,
+    size,
+  );
+
+  // Return the ImageResponse object (use it as needed)
+  return imageResponse;
 }

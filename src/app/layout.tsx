5149fa7002fe5c585cdd01cad5bc2f9a876f42c5
Pedro Laurenti <pedrolaurenti04@gmail.com>
2024-05-06 17:38:27 -0300

    0.1.7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import RootLayout from '../RootLayout/RootLayout'
 import "./globals.scss";
 import {WEBSITE_NAME,META_SEO_KEYWORDS,META_DESCRIPTION} from '@/constants/_APP_SETUP'
 import GoogleAnalytics from "../google/GoogleAnalytics";
-import MetricoolAnalytics from "../google/MetricoolAnalytics";
+// import MetricoolAnalytics from "../google/MetricoolAnalytics";
 // export const revalidate = 60 
 export const metadata: Metadata = {
   metadataBase: new URL('https://artigos.therapieslovekids.com'),
@@ -70,7 +70,7 @@ export default function Layout({ children }:  {children: any}) {
 
         {
           process.env.NEXT_PUBLIC_Metricool_Analytics_TOKEN ? (
-            <MetricoolAnalytics ga_id={process.env.NEXT_PUBLIC_Metricool_Analytics_TOKEN} />
+            // <MetricoolAnalytics ga_id={process.env.NEXT_PUBLIC_Metricool_Analytics_TOKEN} />
           ) : null
         }
         <GoogleAdsAnalytics/>

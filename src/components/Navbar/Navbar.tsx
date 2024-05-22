e89dc2e9a03f0d898d13fef4c313b0ca625be1a5
Pedro <pedrolaurenti04@gmail.com>
2024-05-22 09:22:24 -0300

    0.2.3

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,6 +14,7 @@ import Image from "next/image";
 import LogoDark from "@publ/logo-dark.svg";
 import LogoWhite from "@publ/logo-white.svg";
 import favIcon from "@publ/images/1-1-favicon.svg"
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
 
@@ -41,6 +42,7 @@ const Navbar = () => {
 
   const { theme, setTheme } = useTheme();
   const [showSearch, setShowSearch] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     setShowSearch(false);
@@ -56,68 +58,57 @@ const Navbar = () => {
 
   return (
     <>
-      <nav
-        className={`${classes.navbar} ${
-          isTransparent ? "h-auto"
-          : "h-auto bg-appGray-100 dark:bg-appBlue-400 "
-        }`}
-      >
-        <div
-          className={combineClasses(
-            classes.navbar__container,
-            "container flex items-center justify-between py-1.5",
-            "px-2"
-          )}
-        >
+      <nav className={`${classes.navbar} ${ isTransparent ? "h-auto" : "h-auto bg-appGray-100 dark:bg-appBlue-400 " }`}>
+        <div className={combineClasses(classes.navbar__container,"container flex items-center justify-between py-1.5","px-2")}>
           <div className="flex items-center">
             <Link href="/" className="text-[22px] font-semibold">
-
-            {isTransparent ? (
-              <>
-                <Image
-                  src={LogoDark}
-                  alt="Therapies Love Kids Logo"
-                  style={{ width: '100px', height: '50px' }}
-                  className="block dark:hidden"
-                />
-                <Image
-                  src={LogoWhite}
-                  alt="Therapies Love Kids Logo"
-                  style={{ width: '100px', height: '50px' }}
-                  className="hidden dark:block"
-                />
-              </>
-              ):(
-
+              {isTransparent ? (
+                <>
+                  <Image
+                    src={LogoDark}
+                    alt="Therapies Love Kids Logo"
+                    style={{ width: '100px', height: '50px' }}
+                    className="block dark:hidden"
+                  />
+                  <Image
+                    src={LogoWhite}
+                    alt="Therapies Love Kids Logo"
+                    style={{ width: '100px', height: '50px' }}
+                    className="hidden dark:block"
+                  />
+                </>
+              ) : (
                 <Image
                   src={favIcon}
                   alt="Therapies Love Kids Logo"
                   style={{ width: '100%', height: '30px' }}
                 />
-                
               )}
             </Link>
           </div>
 
-
-          <div className="flex items-start">
-            <button
-              name="theme-switch"
-              aria-label="theme button"
-              className={combineClasses(
-                classes.theme_switch,
-                "pl-3 dark:text-white text-black"
-              )}
-              onClick={changeTheme}
-            >
-              {theme && theme === "dark" ? (
-                <BsFillSunFill className="text-2xl" />
-              ) : (
-                <BsFillMoonFill className="text-md " />
-              )}
-            </button>
+          <div
+            className={`block md:hidden transform transition-transform duration-300 w-full 
+            ${isMenuOpen ? 'rotate-90' : 'rotate-0'}`}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <FaBars className="text-2xl fill-appBlue-100" />
           </div>
-        </div>{" "}
+
+          <div className="flex flex-row justify-end w-full h-full gap-10">
+            <Link href="/">
+              <h3 className={`text-appGray-300 hover:text-appBlue-100 dark:text-appGray-100 dark:hover:text-appBlue-50 text-2xl`}>Início</h3>
+            </Link>
+            <Link href="/sobre">
+              <h3 className={`text-appGray-300 hover:text-appBlue-100 dark:text-appGray-100 dark:hover:text-appBlue-50 text-2xl`}>Sobre</h3>
+            </Link>
+            <Link href="/series">
+              <h3 className={`text-appGray-300 hover:text-appBlue-100 dark:text-appGray-100 dark:hover:text-appBlue-50 text-2xl`}>Séries</h3>
+            </Link>
+          </div>
+
+        </div>
+
         <div className='w-full h-1.5 flex'>
             <div className='w-full hover:w-[150%] bg-appRed-100 duration-75'></div>
             <div className='w-full hover:w-[150%] bg-appYellow-100 duration-75'></div>

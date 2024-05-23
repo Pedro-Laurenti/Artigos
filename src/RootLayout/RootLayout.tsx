7dd3229fe909739fa98903e28ea7884714aa6afe
Pedro <pedrolaurenti04@gmail.com>
2024-05-23 17:15:07 -0300

    0.2.7

--- src/RootLayout/RootLayout.tsx
+++ src/RootLayout/RootLayout.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { ThemeProvider } from "next-themes";
 import { Footer , Navbar } from '@/components';
-import SmoothScrollbar from "@/utils/smoothscroll";
 
 export default function RootLayout({
   children,
@@ -11,15 +10,11 @@ export default function RootLayout({
 
   return (
     <div className="flex flex-col min-h-screen">
-      <SmoothScrollbar>
-
-      
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar />    
               {children}
           <Footer />
         </ThemeProvider>
-      </SmoothScrollbar>
     </div>
       
   );
